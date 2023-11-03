deaa9e6fcb7f727a315f650b390242ca2bc856ce
Delaney Gillilan <delaneygillilan@gmail.com>
2023-11-02 20:18:30 -0700

    fix avatars

--- gomps/components.go
+++ gomps/components.go
@@ -3,41 +3,48 @@ package gomps
 import (
 	"fmt"
 
+	"github.com/delaneyj/toolbelt"
 	"github.com/zeebo/xxh3"
 )
 
-func MiniAvatar(seed string, saturation, lightness uint8, colorCount int, children ...NODE) NODE {
-	h := int(xxh3.HashString(seed) & 0x7fffffffffffffff)
+func MiniAvatar(seed string, saturation, lightness uint8, colorCount, gridSize int, children ...NODE) NODE {
+	cellCount := gridSize * gridSize
+	h := int(xxh3.HashString(seed))
 
 	colorCountF := float32(colorCount)
-	hue := float32(h%colorCount) * (360 / colorCountF)
+	hue := toolbelt.Fit(float32(h%colorCount), 0, colorCountF, 0, 360)
 
-	inner := make([]NODE, 25)
-	for i := 0; i < 25; i++ {
+	half := 0.5
+	inner := make([]NODE, 0, cellCount)
+	for y := 0; y < gridSize; y++ {
+		for x := 0; x < gridSize; x++ {
+			o := y*gridSize + x
 
-		if h&(1<<i%15) != 0 {
-			x := i / 5
-			if i > 14 {
-				x = 7 - x
+			// check if o bit is set
+			if (h>>uint(o))&1 == 1 {
+				inner = append(inner, CIRCLE(
+					CX(float64(x)+half),
+					CY(float64(y)+half),
+					R(half),
+					// WIDTH(1),
+					// HEIGHT(1),
+				))
 			}
-			y := i % 5
-			inner[i] = RECT(
-				X(float64(x)),
-				Y(float64(y)),
-				WIDTH(1),
-				HEIGHT(1),
-			)
 		}
 	}
 
+	hsl := fmt.Sprintf("hsl(%f %d%% %d%%)", hue, saturation, lightness)
+
 	return SVG(
-		VIEWBOXF(-1.5, -1.5, 8, 8),
-		FILL(fmt.Sprintf("hsl(%f %d%% %d%%)", hue, saturation, lightness)),
+		VIEWBOXF(0, 0, float64(gridSize), float64(gridSize)),
+		FILL(hsl),
+		// STROKE(hsl),
+		// STROKEWIDTH(0.1),
 		GRP(inner...),
 		GRP(children...),
 	)
 }
 
 func MiniAvatarDefault(seed string, children ...NODE) NODE {
-	return MiniAvatar(seed, 95, 45, 9, children...)
+	return MiniAvatar(seed, 95, 45, 8, 5, children...)
 }
